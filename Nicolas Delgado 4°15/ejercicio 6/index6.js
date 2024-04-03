a30391db1e99aa1721e84758e9668da0a6725f9f
NicoDelgadoHUB <ncklsdelgado@gmail.com>
2024-04-03 14:56:41 -0300

    ejercicio 6

--- Nicolas Delgado 4°15/ejercicio 6/index6.js
+++ Nicolas Delgado 4°15/ejercicio 6/index6.js
@@ -1,10 +1,25 @@
 function ejercicio6() {
 
-    let = num;
-    if(num % 2 == 0) {
-        console.log("hola mundo");
+   let par = prompt("Ingres un numero para verificar si es par o impar");
+
+   let result = paroImp(par);
+
+   console.log(result);
+}
+
+function paroImp(num){
+    if(num == 0){
+        return "El numero es invalido";
+
     }
+
+    if(num % 2 == 0){
+        return "El numero es par";
+    }
+    else{
+        return "El numero es impar";
+    }
+}
 
     
     
-}
