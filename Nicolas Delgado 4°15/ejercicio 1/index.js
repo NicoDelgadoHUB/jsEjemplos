7afa63296e5de84d347ee45c169f2ffd78ed3a15
melfi-santi <ncklsdelgado@gmail.com>
2024-04-08 13:54:06 -0300

    Delete index.js

--- Nicolas Delgado 4°15/ejercicio 1/index.js
+++
@@ -1,8 +0,0 @@
-function ejercicio1() {
-    let meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
-
-    for (let i = 0; i < meses.length; i++) {
-        console.log(meses[i]);
-
-    }
-}
