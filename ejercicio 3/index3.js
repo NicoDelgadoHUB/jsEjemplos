ffde3d8ea287e214f2af6caec2ba1b245d9a59b2
melfi-santi <ncklsdelgado@gmail.com>
2024-04-08 17:54:48 -0300

    Create index3.js

---
+++ ejercicio 3/index3.js
@@ -0,0 +1,17 @@
+function ejercicio3() {
+    var num1 = 5;
+    var num2 = 8;
+    if(num1<num2) {
+    console.log("numero1 no es mayor que numero2");
+    }
+    if(num2>0) {
+    console.log("numero2 es positivo");
+    }
+    if(nume1!=0) {
+    console.log("numero1 es negativo o distinto de cero");
+    }
+    if(num1++<num2) {
+    console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+    }
+
+}
