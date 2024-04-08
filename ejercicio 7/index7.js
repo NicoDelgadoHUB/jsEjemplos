273c33c3ddd4227a54060a8a66331a29842504a0
melfi-santi <ncklsdelgado@gmail.com>
2024-04-08 17:54:19 -0300

    Create index7.js

---
+++ ejercicio 7/index7.js
@@ -0,0 +1,32 @@
+function ejercicio7() {
+
+    let letra = prompt("Ingresa un letra: ");
+
+  let result = caramaymi(letra) 
+
+  console.log(result);
+
+}
+function caramaymi(txt){
+    let may = 0;
+    let min = 0;
+
+    for(let i = 0; i < txt.lenght; i++){
+
+    if(txt.charAt(i) === txt.charAt(i).toUpperCase()) {
+         may++;
+    } else if(txt.charAt(i) === txt.charAt(i).toLowerCase()) {
+        min++;
+    }
+    }
+
+    if(may == 0){
+        return "son todas minusculas";
+    }else if (min == 0){
+        return "son todas mayusculas";
+    }else{
+        return "son todas mayusculas y minusculas";
+    }
+    
+
+}
